38caa0729bae9421b14c3bad34638187ee3c1439
pavankr401 <106601380+pavankr401@users.noreply.github.com>
2022-11-09 09:47:58 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,34 +2,58 @@ let express = require('express');
 let app = express();
 
 
+let express = require('express');
+let app = express();
+
+// app.use(function(req, res, next)
+//         {
+//           var string = req.method + " " + req.path + " - " + req.ip;
+//           console.log(string);
+
+//           next()
+//         });
+
+app.get("/now", 
+        function(req, res, next)
+        {
+          var output = new Date().toString;
+          req.time = output;
+
+          next();
+        },
+       function(req, res)
+        {
+          res.json({"time": req.time});
+          res.send({"time": req.time})
+        })
+
+app.get("/", 
+        function(req, res)
+        {
+          res.sendFile(__dirname + "/views/index.html");
+        })
 
 
+app.use("/public", express.static(__dirname + "/public"));
 
 
+app.get("/json",
+       function(req, res)
+        {
+          var response = "Hello json";
+          
+            if (process.env.MESSAGE_STYLE === "uppercase")
+            {
+              res.json({"message": response.toUpperCase()});
+            }
+            else
+            {
+              res.json({"message": response});
+            }
+          
+        });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(new Date());
 
 
 
